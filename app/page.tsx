c75a38e13755e82f7edba834cfe646d67e8ff312
avla-byte <avla-byte@users.noreply.github.com>
2025-12-28 19:23:58 +0300

    Update .gitignore to streamline ignored files and directories, removing unnecessary entries and organizing sections for dependencies, testing, production, and local environment files.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="main">
+      <div className="container">
+        <h1>Добро пожаловать в Next.js!</h1>
+        <p>Минимальное приложение успешно запущено.</p>
+      </div>
+    </main>
+  )
+}
+
